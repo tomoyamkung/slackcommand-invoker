08b26c728208397c75c4c3636a56b83535cbaea1
tomoyamkung <tsuyoshi.sugiyama@gmail.com>
2022-08-11 17:59:15 +0900

    :recycle:mypy や flake8 を適用

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -2,36 +2,43 @@ import json
 import logging
 import os
 import urllib.parse
-from typing import List
+from typing import List, Optional
 
 import boto3
 
-def lambda_handler(event: dict, context):
+
+def lambda_handler(event: dict[str, str], context):
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
-
     logger.info(json.dumps(event))
 
     if "challenge" in event:
         return event.get("challenge")
 
-    logger.info(json.dumps(event.get("body")))
+    params: Optional[List[str]] = parse_params(event.get("body"))
+    if params is not None:
+        logger.info({"params": params})
 
-    text_params: List["str"] = [
-        element for element in event.get("body").split("&") if element.startswith("text=")
-    ]
-    params: List["str"] = urllib.parse.unquote_plus(text_params[0].removeprefix("text=")).split(" ")
-    logger.info({"params": params})
-
-    client = boto3.client('lambda')
+    client = boto3.client("lambda")
     response = client.invoke(
         FunctionName=os.environ["LAMBDA_FUNCTION_NAME"],
         InvocationType="Event",
         LogType="Tail",
-        Payload=json.dumps({"params": params})
+        Payload=json.dumps({"params": params}),
     )
+    logger.info(response)
 
-    return {
-        "statusCode": 200,
-        "body": json.dumps("OK")
-    }
+    return {"statusCode": 200, "body": json.dumps("OK")}
+
+
+def parse_params(params_string: Optional[str]) -> Optional[List[str]]:
+    if params_string is None:
+        return None
+
+    text_params: List["str"] = [
+        element for element in params_string.split("&") if element.startswith("text=")
+    ]
+    if len(text_params) == 0:
+        return None
+
+    return urllib.parse.unquote_plus(text_params[0].removeprefix("text=")).split(" ")
